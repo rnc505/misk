0c189d82141ed644e4496440a5daf0ac061a480d
Robby Cohen <robbyc@squareup.com>
2026-07-20 16:33:55 -0500

    ci: resolve exclusively via Block Artifactory mirror for cash-server composite validation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ import org.gradle.plugins.ide.idea.model.IdeaModel
 pluginManagement {
   repositories {
     maven(url = "https://global.block-artifacts.com/artifactory/maven-cash-backend")
-    mavenCentral()
-    gradlePluginPortal()
   }
 }
 
@@ -25,7 +23,6 @@ dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     maven(url = "https://global.block-artifacts.com/artifactory/maven-cash-backend")
-    mavenCentral()
     maven(url = "https://s3-us-west-2.amazonaws.com/dynamodb-local/release")
   }
 }
